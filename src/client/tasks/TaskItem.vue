7e70391c4cd6b69d5b91d3ea03eaa75167be3ae7
doannucphys <doan.phys@gmail.com>
2019-10-19 02:23:34 +0200

    test algo

--- src/client/tasks/TaskItem.vue
+++ src/client/tasks/TaskItem.vue
@@ -22,8 +22,8 @@
       </b-col>
      
       <b-col cols="12" md="3">
-        <span v-if="timeToDeadline > 0">{{days}}d-{{hours}}h-{{ minutes}}m-{{ seconds}}s</span>
-        <span v-else class='over-deadline'>Over deadline</span>
+        <span v-if="isOverDeadline" class='over-deadline'>Over deadline</span>
+        <span v-else>{{days}}d-{{hours}}h-{{ minutes}}m-{{ seconds}}s</span>
       </b-col>
 
       <b-col cols="12" md="3">
@@ -71,15 +71,16 @@ export default {
       taskDeadline: '',
 
       clock: 1000*20,
+      deltaTime: 3*1000*20,
+
       noticed: false,
+      isOverDeadline: false,
 
       interval: null,
       days: 0,
       hours: 0,
       minutes: 0,
       seconds: 0,
-      markDiffTime: '',
-      timeToDeadline: '',
       intervals: {
         second: 1000,
         minute: 1000 * 60,
@@ -112,22 +113,35 @@ export default {
     updateDiffs(onCreated) {
       if (this.taskData && this.taskData.deadline) {
         let diff = (new Date(this.taskDeadline)).getTime() - (new Date()).getTime();
-        this.markDiffTime = diff;
-        if (diff > 0) {
-          this.timeToDeadline = diff;
-          this.days = Math.floor(diff / this.intervals.day);
-          diff -= this.intervals.day * this.days;
-          this.hours = Math.floor(diff / this.intervals.hour);
-          diff -= this.intervals.hour * this.hours;
-          this.minutes = Math.floor(diff / this.intervals.minute);
-          diff -= this.intervals.minute * this.minutes;
-          this.seconds = Math.floor(diff / this.intervals.second);
+        let modDiff = diff;
+        if (modDiff > 0) {
+          this.days = Math.floor(modDiff / this.intervals.day);
+          modDiff -= this.intervals.day * this.days;
+          this.hours = Math.floor(modDiff / this.intervals.hour);
+          modDiff -= this.intervals.hour * this.hours;
+          this.minutes = Math.floor(modDiff / this.intervals.minute);
+          modDiff -= this.intervals.minute * this.minutes;
+          this.seconds = Math.floor(modDiff / this.intervals.second);
         }
-        if (!onCreated && !this.noticed && (!this.timeToDeadline || (Math.abs(this.timeToDeadline) < 3*this.clock)) ) {
-          this.$emit('overDeadline');
-          this.noticed = true;
-        } else if (onCreated && (this.markDiffTime/this.intervals.day) >= - 1 && this.markDiffTime <= 0) {
-          this.$emit('overDeadline');
+        if (!onCreated && !this.noticed) {
+          // auto notice when time comming up
+          if (Math.abs(diff) <= this.deltaTime) {
+            this.noticed = true;
+            this.isOverDeadline = true;
+            this.$emit('overDeadline');
+          } else if (diff < 0 && Math.abs(diff) > this.deltaTime) {
+            this.isOverDeadline = true;
+          } else {
+            this.isOverDeadline = false;
+          }
+        } else if (onCreated) {
+          // notice when open page, only notice when deadline is in current or previous day
+          if ((diff <= 0 && (diff/this.intervals.day >= - 1))  || (Math.abs(diff) < this.deltaTime)) {
+            this.isOverDeadline = true;
+            this.$emit('overDeadline');
+          } else if (diff <= 0) {
+            this.isOverDeadline = true;
+          }
         }
       }
     },
@@ -141,8 +155,6 @@ export default {
           task: this.taskEdit,
           deadline: new Date(this.taskDeadline)
         }});
-        this.newTask = '';
-        this.deadline = '';
         this.noticed = false;
         this.updateDiffs(false);
       }
